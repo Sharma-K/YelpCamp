4503123385eec99c4ffebc8e88bbc4fc121c4fcf
Keshav Sharma <keshav2743@gmail.com>
2022-08-15 15:41:49 +0530

    changed DB_URL

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const helmet = require('helmet');
 const MongoDBStore = require('connect-mongo');
 
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL;
 
 
 const app = express();
